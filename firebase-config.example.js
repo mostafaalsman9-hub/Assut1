cc213be11afa75f1daac62e0bd2e86f27f66fa6d
mostafaalsman9-hub <mostafaalsman9@gmail.com>
2026-09-19 17:41:29 +0300

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,3 +0,0 @@
-// The real browser config is already in js/firebase.js.
-// This file documents the project without containing any server credentials.
-export const firebaseProjectId = 'drali-f6b14';
